5f56a1a97cafbf77ee9fb7f5e000ec4e04768929
daja0923 <djjamol@gmail.com>
2016-06-22 23:14:44 +0200

    Refactored GameHistory. Tracking state moved out to a new object

--- src/main/scala/engine/GameHistory.scala
+++ src/main/scala/engine/GameHistory.scala
@@ -1,50 +1,20 @@
 package engine
 
-import engine.pieces.Piece
 import engine.squares.Square
 
 /**
  * Created by jamol on 20/06/16.
  */
-class GameHistory( moves:List[Move]) {
+class GameHistory(moves:List[Move]) {
+
+  type State = List[Square]
 
   val initialState:State = start
-
 
 
   def start:State = Square.initiate
 
 
+  def endState:State = StateTracker.trackState(initialState, moves.reverse)
 
-  type State = List[Square]
-
-
-
-  def endState:State = trackState(initialState, moves.reverse)
-
-
-
-  def trackState(state: State, moves: List[Move]):State = {
-    if(moves.isEmpty) state
-    else trackState(changeState(state, moves.head), moves.tail)
-  }
-
-
-
-  def changeState(current:State, move: Move):State = {
-    val square = current.find(s => s.position equals move.dest).get
-    current.map { s =>
-      if (s.position equals move.dest) s.copy(position = move.dest)
-      else if(piecesTheSame(s.piece, move.removedEnemyPiece)) s.copy(piece = None)
-      else s
-    }
-  }
-
-
-  def piecesTheSame(piece1:Option[Piece], piece2:Option[Piece]):Boolean =
-    (piece1, piece2) match {
-      case (None, _) => false
-      case (_, None) => false
-      case (Some(p1), Some(p2)) => p1 equals p2
-    }
 }
---
+++ src/main/scala/engine/StateTracker.scala
@@ -0,0 +1,48 @@
+package engine
+
+import engine.pieces.Piece
+import engine.squares.Square
+
+/**
+ * Created by jamol on 22/06/16.
+ */
+object StateTracker {
+
+  type State = List[Square]
+
+  /**
+   * tracks from given state the end of the moves
+   * Note moves must be in in order (first move on head, etc)
+   * @param state  List[Square]
+   * @param moves  List[Move]
+   * @return Returns the end state of squares
+   */
+  @scala.annotation.tailrec
+  def trackState(state: State, moves: List[Move]):State =
+    moves match {
+      case Nil => state
+      case m::ms => trackState(changeState(state, m), ms)
+  }
+
+
+
+
+  def changeState(current:State, move: Move):State =
+    current.map { square =>
+      if (square.position equals move.dest)
+        square.copy(piece = Some(move.piece))
+      else if(square.position equals move.from)
+        square.copy(piece = None)
+      else if(piecesTheSame(square.piece, move.removedEnemyPiece))
+        square.copy(piece = None) //clearing removedEnemy square explicitly because it is not always destination square
+      else square
+    }
+
+
+  private def piecesTheSame(piece1:Option[Piece], piece2:Option[Piece]):Boolean =
+    (piece1, piece2) match {
+      case (None, _) => false
+      case (_, None) => false
+      case (Some(p1), Some(p2)) => p1 equals p2
+    }
+}
